35035415249b4518b3bb1fe4f21ebfa7893b6bed
Hosain <cryptichasan925@gmail.com>
2025-06-20 13:02:14 +0600

    quardic n

---
+++ Time_Space_Complexity/quardic_complexity_.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int n;
+   cin >> n;
+   for(int i = 0; i < n; i++){
+     for(int j = 0; j < n; j++){
+       cout << "Hello" << endl;
+     }
+   }
+   return 0;
+};
+
+// This is order of n^2 complexity, which is O(n^2).
