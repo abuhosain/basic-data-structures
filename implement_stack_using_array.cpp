201bdbf2290b8ec7e9b8707e8daf1ac71ab900df
Hosain <cryptichasan925@gmail.com>
2025-07-26 13:40:39 +0600

    push for stack

---
+++ implement_stack_using_array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStck
+{
+public:
+   vector<int> v;
+   void push(int val)
+   {
+      v.push_back(val);
+   }
+};
+
+int main()
+{
+
+   return 0;
+};
