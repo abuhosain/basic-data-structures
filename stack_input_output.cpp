3abc4b0a194eac7b73dee55b86a42c0fc92a5be6
Hosain <cryptichasan925@gmail.com>
2025-07-26 14:05:40 +0600

    stack input outpu

--- stack_input_output.cpp
+++ stack_input_output.cpp
@@ -4,44 +4,58 @@ using namespace std;
 class my_stack
 {
 public:
-   vector<int> v;
-   void push(int val)
-   {
-      v.push_back(val);
-   }
-   void pop()
-   {
-      if (!v.empty())
-      {
-         v.pop_back();
-      }
-      else
-      {
-         cout << "Stack is empty, cannot pop." << endl;
-      }
-   }
+  vector<int> v;
+  void push(int val)
+  {
+    v.push_back(val);
+  }
+  void pop()
+  {
+    if (!v.empty())
+    {
+      v.pop_back();
+    }
+    else
+    {
+      cout << "Stack is empty, cannot pop." << endl;
+    }
+  }
 
-   int top()
-   {
-      if (!v.empty())
-      {
-         return v.back();
-      }
-   }
+  int top()
+  {
+    if (!v.empty())
+    {
+      return v.back();
+    }
+  }
 
-   int size()
-   {
-      return v.size();
-   }
+  int size()
+  {
+    return v.size();
+  }
 
-   bool empty()
-   {
-      return v.empty();
-   }
+  bool empty()
+  {
+    return v.empty();
+  }
 };
 
 int main()
 {
-   my_stack st;
-   return 0;
+  my_stack st;
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    int x;
+    cin >> x;
+    st.push(x);
+  }
+  while (st.empty() == false)
+  {
+    cout << st.top() << endl;
+    st.pop();
+  }
+
+  return 0;
 };
