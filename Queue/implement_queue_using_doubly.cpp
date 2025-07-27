cedd4d11c7608d7c898548c58a6ebf12364d2912
Hosain <cryptichasan925@gmail.com>
2025-07-28 00:58:43 +0600

    queue implement using doubly linklist

---
+++ Queue/implement_queue_using_doubly.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *next;
+  Node *prev;
+  Node(int val)
+  {
+    this->val = val;
+    this->next = NULL;
+    this->prev = NULL;
+  }
+};
+
+class myQuee
+{
+public:
+  Node *head = NULL;
+  Node *tail = NULL;
+  int sz = 0;
+
+  void push(int val)
+  {
+    sz++;
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+      head = newNode;
+      tail = newNode;
+      return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+  };
+
+  void pop()
+  {
+    sz--;
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+    if (head == NULL)
+    {
+      tail = NULL;
+      return;
+    }
+    head->prev = NULL;  
+  };
+
+  int front()
+  {
+    return head->val;
+  };
+
+  int back()
+  {
+    return tail->val;
+  };
+
+  int size()
+  {
+    return sz;
+  };
+
+  bool empty()
+  {
+    if (head == NULL)
+      return true;
+    else
+      return false;
+  }
+};
+
+int main()
+{
+  myQuee q;
+  int n;
+  cin >> n;
+  for(int i = 0; i < n; i++)
+  {
+    int x;
+    cin >> x;
+    q.push(x);
+  };
+
+  cout << q.front() << " " << q.back() << " " << q.size() << endl; 
+
+  while (!q.empty())
+  {
+    cout << q.front() << endl;
+    q.pop();
+  };
+  return 0;
+};
