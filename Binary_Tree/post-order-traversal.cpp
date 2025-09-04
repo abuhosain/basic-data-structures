abc758e0d9500b857fcd59b4d795b424460db6e5
Hosain <cryptichasan925@gmail.com>
2025-09-04 13:22:37 +0600

    post order

---
+++ Binary_Tree/post-order-traversal.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int val;
+  Node *left;
+  Node *right;
+  Node(int val)
+  {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+void postOrder(Node *root)
+{
+  if (root == NULL)
+    return;
+  postOrder(root->left);
+  postOrder(root->right);
+  cout << root->val << " ";
+}
+
+int main()
+{
+  Node *root = new Node(1);
+  Node *a = new Node(2);
+  Node *b = new Node(3);
+  Node *c = new Node(4);
+  Node *d = new Node(5);
+  Node *e = new Node(6);
+  root->left = a;
+  root->right = b;
+  a->left = c;
+  b->left = d;
+  b->right = e;
+
+  postOrder(root);
+
+  return 0;
+};
